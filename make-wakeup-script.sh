c306b15a288aa887c0119a09e31359d16b7260e2
elsehow <yes@cosmopol.is>
2016-02-17 20:55:04 -0800

    added prompt for daily goals

--- make-wakeup-script.sh
+++ make-wakeup-script.sh
@@ -4,20 +4,23 @@ function whitespace () {
   echo ""
 }
 
-echo "# what did you dream?"
+echo "what did you dream?"
 whitespace
 
-echo "## the poetic edda"
+echo "what are your goals for the day?"
+whitespace
+
+echo "the poetic edda"
 node storyteller.js -s havamal.txt -l havamal.log
 whitespace
 
 bash org-agenda.sh
 whitespace
 
-echo "## weather berkeley ca"
+echo "=weather berkeley ca="
 node weather.js
 whitespace
 
-echo "## the nyt"
+echo "the nyt"
 node top-nyt.js
 whitespace
